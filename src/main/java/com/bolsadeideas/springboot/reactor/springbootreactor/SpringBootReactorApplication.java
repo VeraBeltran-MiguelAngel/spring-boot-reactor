c9458af0ca6cd20fbdbebb704eb54f391b779f9c
Miguel Angel Vera Beltran <miguel-angel.vera-beltran@outlook.com>
2023-10-05 11:28:57 -0600

    Creamos proyecto con Reactor API
    Se coloca dependencia de reactor api en el pom.xml desde su pagina oficial

---
+++ src/main/java/com/bolsadeideas/springboot/reactor/springbootreactor/SpringBootReactorApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.reactor.springbootreactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+}
